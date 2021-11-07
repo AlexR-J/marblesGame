aae730cbb32e464230f5fb439d6dc52ed4a767f1
AlexR-J <61548577+AlexR-J@users.noreply.github.com>
2021-11-07 21:58:31 +0000

    Add files via upload

--- Bag.java
+++ Bag.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Bag {
 
@@ -23,4 +24,37 @@ public class Bag {
     return pebbles;
   }
 
+  public synchronized ArrayList<Pebble> giveHand() {
+    //create a temporary arraylist to contain the pebbles to be given to a player.
+    ArrayList<Pebble> temp = new ArrayList<Pebble>();
+    Random rand = new Random();
+    //10 times generate a random number and choose the pebble and that index.
+    //then add this pebble to the temprary arraylist.
+    for(int i = 0; i<10; i++) {
+      int randomNumber = rand.nextInt(pebbles.size());
+      temp.add(pebbles.get(randomNumber));
+    }
+    //return the list of pebbles now containing 10 pebbles.
+    return temp;
+  }
+
+  public synchronized Pebble giveRandomPebble() {
+      //generate a randm number between 0 and the number of pebles in the bag
+      //then use this to choose a new pebble and return it to the player.
+      Random rand = new Random();
+      int randomNumber = rand.nextInt(pebbles.size());
+      Pebble temp = pebbles.get(randomNumber);
+      return temp;
+  }
+
+  public synchronized ArrayList<Pebble> takePebble(ArrayList<Pebble> playersHand) {
+      //generate a random number between 0 and the number of pebbles in the
+      //players hand. Then use that to remove a pebble from the players hand,
+      //and return it to them.
+      Random rand = new Random();
+      int randomNumber = rand.nextInt(playersHand.size());
+      Pebble temp = playersHand.get(randomNumber);
+      playersHand.remove(temp);
+      return playersHand;
+  }
 }
